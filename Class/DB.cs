ddb291c3cb198dbeea9a3d37ce59b2b20cb5ccfa
DmitriyNikolaev3802 <79700212+DmitriyNikolaev3802@users.noreply.github.com>
2022-06-03 11:02:12 +0300

    Исправление ошибок

---
+++ Class/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryInfoSystemLite_NikolaevDV.Class
+{
+    internal class DB
+    {
+        public static LibraryISEntities db = new LibraryISEntities();
+    }
+}
